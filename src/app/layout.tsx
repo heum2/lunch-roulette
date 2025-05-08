94a69b520e8a622cfcedb68ec0e8606df0cb859f
heum2 <heum2.dev@gmail.com>
2025-05-08 13:59:42 +0900

    Fix: 디자인 테마 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" data-theme="caramellatte">
       <body className={pretendard.className}>{children}</body>
     </html>
   );
